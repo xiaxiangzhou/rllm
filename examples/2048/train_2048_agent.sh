8f6d6f2e6989918a6eff9a5ecb31bb5d50cf2467
xiangzhou xia <xiaxiangzhou@gmail.com>
2025-10-25 22:36:09 -0700

    rebase

--- examples/2048/train_2048_agent.sh
+++ examples/2048/train_2048_agent.sh
@@ -13,8 +13,8 @@ RLLM_DIR=$(python3 -c "import rllm; import os; print(os.path.dirname(os.path.dir
 
 python3 -m examples.2048.train_2048_agent \
     algorithm.adv_estimator=grpo \
-    data.train_batch_size=8 \
-    data.val_batch_size=16 \
+    data.train_batch_size=1 \
+    data.val_batch_size=1 \
     data.max_prompt_length=4096 \
     data.max_response_length=2048 \
     actor_rollout_ref.model.path=Qwen/Qwen2.5-3B-Instruct \
@@ -22,8 +22,8 @@ python3 -m examples.2048.train_2048_agent \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.loss_agg_mode=seq-mean-token-sum \
-    actor_rollout_ref.actor.ppo_mini_batch_size=8 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=2 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=1 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=24000 \
     actor_rollout_ref.actor.use_kl_loss=False \
@@ -39,10 +39,10 @@ python3 -m examples.2048.train_2048_agent \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.mode="async" \
     actor_rollout_ref.rollout.chat_scheduler=verl.schedulers.completions_scheduler.CompletionsScheduler \
-    actor_rollout_ref.rollout.enforce_eager=False \
+    actor_rollout_ref.rollout.enforce_eager=True \
     actor_rollout_ref.rollout.temperature=0.7 \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.85 \
-    actor_rollout_ref.rollout.n=4 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
+    actor_rollout_ref.rollout.n=2 \
     actor_rollout_ref.rollout.val_kwargs.n=2 \
     actor_rollout_ref.rollout.val_kwargs.temperature=0.7 \
     actor_rollout_ref.rollout.val_kwargs.top_p=0.8 \
